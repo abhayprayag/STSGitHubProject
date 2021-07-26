858b136ee62ffdcfb664081d8a9ea498f641555a
ADMIN <ADMIN@host.docker.internal>
2021-07-26 16:21:15 +0530

    first commit

---
+++ STSGitHubProject/src/main/java/com/abhay/in/STSGitHubProject/App.java
@@ -0,0 +1,13 @@
+package com.abhay.in.STSGitHubProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
